464500bb07703008002fb1bb68b886cf541b927f
Your Name <1218866243@qq.com>
2018-06-03 11:51:23 +0800

    '英雄列表'

---
+++ src/components/mysidebar.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="col-sm-3 col-md-2 sidebar">
+        <ul class="nav nav-sidebar">
+          <router-link tag="li" to="/heroes" active-class="active"><a>英雄管理</a></router-link>
+          <router-link tag="li" to="/weapons" active-class="active"><a>武器管理</a></router-link>
+          <router-link tag="li" to="/equips" active-class="active"><a>装备管理</a></router-link>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/views/heroes/list.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <h2 class="sub-header">英雄列表</h2>
+    <a class="btn btn-success" href="add.html">Add</a>
+    <div class="table-responsive">
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>姓名</th>
+            <th>性别</th>
+            <th>操作</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(item, index) in list" :key="item.id">
+            <td>{{ index }}</td>
+            <td>{{ item.name }}</td>
+            <td>{{ item.gender }}</td>
+            <td>
+              <a href="edit.html">edit</a>
+              &nbsp;&nbsp;
+              <a href="javascript:window.confirm('Are you sure?')">delete</a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+// 导入axios模块
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      list: []
+    };
+  },
+  mounted() {
+    // 发送请求
+    axios.get('http://localhost:3000/heroes')
+      .then((res) => {
+        if (res.status === 200) {
+          this.list = res.data;
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+};
+</script>
+
+<style>
+
+</style>
